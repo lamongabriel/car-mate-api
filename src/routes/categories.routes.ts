f3c6e9e3ba9810782d36fea9a175d6b989ddbbef
Gabriel Lamon <lamongabriel@icloud.com>
2023-10-09 10:44:47 -0300

    fix/categoryRoutes - create validation by name and my missing params

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -8,6 +8,18 @@ const categoriesRoutes = Router();
 categoriesRoutes.post("/", (req: Request, res: Response) => {
 	const { name, description } = req.body;
 
+	if (!name || !description) {
+		return res.status(400).json({ message: "Missing NAME or DESCRIPTION" });
+	}
+
+	const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+	if (categoryAlreadyExists) {
+		return res
+			.status(400)
+			.json({ message: `Category with name '${name}' already exists` });
+	}
+
 	const newCategory = categoriesRepository.create({ name, description });
 
 	return res.status(201).send(newCategory);

3190558c6ae3629ead8243a60c78e8fc0710ddcd
Gabriel Lamon <lamongabriel@icloud.com>
2023-10-09 10:33:36 -0300

    CategoriesRepository + CategoriesRepository create method

---
+++ src/repositories/CategoriesRepository.ts
@@ -0,0 +1,28 @@
+import Category from "../model/Category";
+
+interface ICreateCategoryDTO {
+	name: string;
+	description: string;
+}
+
+export default class CategoriesRepository {
+	private categories: Category[];
+
+	constructor() {
+		this.categories = [] as Category[];
+	}
+
+	create({ name, description }: ICreateCategoryDTO): Category {
+		const newCategory: Category = new Category();
+
+		Object.assign(newCategory, {
+			name,
+			description,
+			created_at: new Date(),
+		});
+
+		this.categories.push(newCategory);
+
+		return newCategory;
+	}
+}
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,23 +1,14 @@
 import { Request, Response, Router } from "express";
 
-import Category from "../model/Category";
+import CategoriesRepository from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
-
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (req: Request, res: Response) => {
 	const { name, description } = req.body;
 
-	const newCategory: Category = new Category();
-
-	Object.assign(newCategory, {
-		name,
-		description,
-		created_at: new Date(),
-	});
-
-	categories.push(newCategory);
+	const newCategory = categoriesRepository.create({ name, description });
 
 	return res.status(201).send(newCategory);
 });

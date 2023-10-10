01f19bde7c0a2b0355444a86236fb7dc04a5d8b4
Gabriel Lamon <lamongabriel@icloud.com>
2023-10-10 16:55:57 -0300

    Create Specification Service

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,3 +1,25 @@
+import SpecificationsRepository from "../repositories/SpecificationsRepository";
+
+interface IRequest {
+	name: string;
+	description: string;
+}
+
 export default class CreateSpecificationService {
-	execute() {}
+	constructor(private specificationRepository: SpecificationsRepository) {}
+
+	execute({ name, description }: IRequest) {
+		if (!name || !description) {
+			throw new Error("Missing required data");
+		}
+
+		const speficiationAlreadyExists =
+			this.specificationRepository.findByName(name);
+
+		if (speficiationAlreadyExists) {
+			throw new Error(`Specification with name '${name}' already exists`);
+		}
+
+		this.specificationRepository.create({ name, description });
+	}
 }

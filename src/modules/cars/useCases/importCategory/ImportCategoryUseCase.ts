0e36680735dfd1b03bf38e857fc45f418c08d850
Gabriel Lamon <lamongabriel@icloud.com>
2023-10-11 16:10:44 -0300

    import category by file basic controller + routine

---
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -0,0 +1,5 @@
+export default class ImportCategoryUseCase {
+	execute(file: any) {
+		console.log(file);
+	}
+}
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,7 +1,12 @@
 import { Router } from "express";
+import multer from "multer";
+import path from "node:path";
 
 import createCategoryController from "../modules/cars/useCases/createCategory";
+import importCategoryController from "../modules/cars/useCases/importCategory";
 import listCategoriesController from "../modules/cars/useCases/listCategories";
+
+const upload = multer({ dest: path.resolve(__dirname, "..", "..", "tmp") });
 
 const categoriesRoutes = Router();
 
@@ -10,7 +15,11 @@ categoriesRoutes.post("/", (request, response) => {
 });
 
 categoriesRoutes.get("/", (request, response) => {
-	listCategoriesController.handle(request, response);
+	return listCategoriesController.handle(request, response);
+});
+
+categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+	return importCategoryController.handle(request, response);
 });
 
 export default categoriesRoutes;

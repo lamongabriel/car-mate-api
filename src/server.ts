a8cb2b0bbed77bbc891d921e3d7e7b67dc06f055
Gabriel Lamon <lamongabriel@icloud.com>
2023-10-11 17:05:41 -0300

    Swagger Docs initialization and configuration

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
-// Modules
 import express from "express";
+import swaggerUI from "swagger-ui-express";
 
 import appRoutes from "./routes/app.routes";
+import swaggerConfig from "./swagger.json";
 
 const app = express();
 app.use(express.json());
 
+// Swagger
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerConfig));
+
+// Routes
 app.use(appRoutes);
 
 app.listen(3000, () => {

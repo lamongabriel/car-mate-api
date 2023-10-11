bb8c02c1f302f2b45edbad18d2b84c4a04d63102
Gabriel Lamon <lamongabriel@icloud.com>
2023-10-10 22:55:36 -0300

    App Routes - Centralized Routes

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,12 @@
 // Modules
 import express from "express";
 
-// Routes
-import categoriesRoutes from "./routes/categories.routes";
-import specificationsRoutes from "./routes/specification.routes";
+import appRoutes from "./routes/app.routes";
 
 const app = express();
 app.use(express.json());
 
-// Categories routes
-app.use("/categories", categoriesRoutes);
-app.use("/specifications", specificationsRoutes);
+app.use(appRoutes);
 
 app.listen(3000, () => {
 	console.log("EXPRESS Server initialized");

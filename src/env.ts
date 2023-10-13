68f995c26153aa5c2d4f9f427e7ed5e3d6060408
Gabriel Lamon <lamongabriel@icloud.com>
2023-10-13 15:48:07 -0300

    created env file

---
+++ src/env.ts
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.resolve(__dirname, "..", ".env") });
+
+const env = {
+	DATABASE_HOST: process.env.DATABASE_HOST,
+	DATABASE_PORT: Number(process.env.DATABASE_PORT),
+	DATABASE_USER: process.env.DATABASE_USER,
+	DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
+	DATABASE_DB: process.env.DATABASE_DB,
+};
+
+Object.values(env).forEach((env) => {
+	if (env === null || env === undefined || Number.isNaN(env)) {
+		throw new Error("Missing required environment variables");
+	}
+});
+
+export default env;
